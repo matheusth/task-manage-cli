c2dd4db4cbd669a9d1feaf55b491dbb58edc8281
Matheus Henrique Passos de Oliveira <matheus.passos@ifgoiano.edu.br>
2022-07-15 10:35:36 -0300

    feat: Group issues in their activities

--- src/issue.rs
+++ src/issue.rs
@@ -5,12 +5,56 @@ mod tests {
   fn issue_new_return_a_issue() {
     let title = String::from("title");
     let description = String::from("description");
-    let issue = Issue::new(&title, &description, 12.4, String::from("22/07/2021"));
+    let issue = Issue::new(
+      title.clone(),
+      description.clone(),
+      12.4,
+      String::from("22/07/2021"),
+    );
 
     assert_eq!(description, issue.description);
     assert_eq!(title, issue.title);
     assert_eq!(12.4, issue.time_spent);
     assert_eq!("22/07/2021", issue.date.as_str());
+  }
+
+  #[test]
+  fn add_issue_to_activity() {
+    let title = String::from("title");
+    let description = String::from("description");
+    let issue = Issue::new(
+      title.clone(),
+      description.clone(),
+      12.4,
+      String::from("22/07/2021"),
+    );
+    let mut activity = Activity::new(
+      String::from("Atendimento ao Usúrario"),
+      String::from("descrição"),
+      40.0,
+    );
+
+    activity.add_issue(issue);
+    assert_eq!(activity.issues.len(), 1);
+  }
+  #[test]
+  fn remove_issue_from_activity() {
+    let title = String::from("title");
+    let description = String::from("description");
+    let issue = Issue::new(
+      title.clone(),
+      description.clone(),
+      12.4,
+      String::from("22/07/2021"),
+    );
+    let mut activity = Activity::new(
+      String::from("Atendimento ao Usúrario"),
+      String::from("descrição"),
+      40.0,
+    );
+    activity.add_issue(issue);
+    activity.remove_issue(0);
+    assert_eq!(activity.issues.len(), 0);
   }
 }
 
@@ -22,12 +66,36 @@ pub struct Issue {
 }
 
 impl Issue {
-  pub fn new(title: &String, description: &String, time_spent: f32, date: String) -> Self {
+  pub fn new(title: String, description: String, time_spent: f32, date: String) -> Self {
     Issue {
-      title: title.clone(),
-      description: description.clone(),
+      title,
+      description,
       time_spent,
       date,
     }
+  }
+}
+
+pub struct Activity {
+  category: String,
+  description: String,
+  planned_time: f32,
+  issues: std::vec::Vec<Issue>,
+}
+
+impl Activity {
+  pub fn new(category: String, description: String, planned_time: f32) -> Activity {
+    Activity {
+      category,
+      description,
+      planned_time,
+      issues: Vec::new(),
+    }
+  }
+  pub fn add_issue(&mut self, issue: Issue) {
+    self.issues.push(issue);
+  }
+  pub fn remove_issue(&mut self, issue_index: usize) {
+    self.issues.swap_remove(issue_index);
   }
 }
