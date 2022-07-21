dfa65b50cc5f1f1a222b4cf366a7efd11518a798
Matheus Henrique Passos de Oliveira <matheus.passos@ifgoiano.edu.br>
2022-07-21 09:55:56 -0300

    refactor: Add file for ui methods

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 pub mod archive;
 pub mod issue;
+pub mod ui;
 
 use archive::*;
-use issue::{Activity, Issue};
-use std::error::Error;
-use std::io::Write;
+use issue::Activity;
+use ui::*;
 
 fn main() {
     let mut activities: std::vec::Vec<Activity> = load_from_file().unwrap_or_else(|error| {
@@ -29,17 +29,7 @@ fn handle_command(activities: &mut std::vec::Vec<Activity>) -> bool {
             _ => false,
         },
         "add tarefa" => {
-            let activities_iter = activities.into_iter();
-            for (index, activity) in activities_iter.enumerate() {
-                println!(
-                    "indice: {:<20}categoria: {:<50}Carga Horária: {:4.2}",
-                    index, activity.category, activity.planned_time
-                );
-                println!("descrição: {}", activity.description);
-                println!(
-                    "=========================================================================="
-                );
-            }
+            print_tasks(&activities);
             let index = get_input("Inidice da atividade: ")
                 .unwrap()
                 .parse::<usize>()
@@ -56,30 +46,4 @@ fn handle_command(activities: &mut std::vec::Vec<Activity>) -> bool {
         "sair" => false,
         _ => true,
     }
-}
-fn create_activity() -> Result<Activity, Box<dyn Error>> {
-    let category = get_input("Categoria: ")?;
-    let description = get_input("Descrição: ")?;
-    let planned_time = get_input("Tempo previsto: ")?.parse::<f32>()?;
-
-    Ok(Activity::new(category, description, planned_time))
-}
-fn create_issue(activity: &mut Activity) -> Result<(), Box<dyn Error>> {
-    let title = get_input("Title:")?;
-    let description = get_input("Descrição: ")?;
-    let time_spent = get_input("Tempo gasto: ")?.parse::<f32>()?;
-    let date = get_input("Data de execução: ")?;
-    activity.add_issue(Issue::new(title, description, time_spent, date));
-
-    Ok(())
-}
-
-fn get_input(message: &str) -> std::io::Result<String> {
-    let mut line = String::new();
-
-    print!("{}", message);
-    std::io::stdout().flush().expect("Failed to flush stdout");
-    std::io::stdin().read_line(&mut line)?;
-
-    Ok(String::from(line.trim()))
 }
