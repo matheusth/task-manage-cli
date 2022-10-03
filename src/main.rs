a1be8f27a556a7a85ab4123a7dad506628411673
Matheus Henrique Passos de Oliveira <matheushtip@gmail.com>
2022-10-03 10:38:48 -0300

    feat(break): remove former command interface

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 pub mod archive;
 pub mod cli;
 pub mod issue;
-pub mod ui;
 
 use archive::*;
 use clap::Parser;
 use cli::*;
-use issue::{Activity, WorkPlan};
-use ui::*;
+use issue::WorkPlan;
 
 fn main() {
     let mut workplans: Vec<WorkPlan> = Vec::new();
@@ -26,44 +24,5 @@ fn main() {
             _ => panic!("Not implemented yet"),
         },
     };
-    save_to_file(&workplans);
-}
-
-fn handle_command(activities: &mut std::vec::Vec<Activity>) -> bool {
-    let option = get_input("Commando >> ").expect("Failed to read command");
-    match option.as_str() {
-        "criar atividade" => match create_activity() {
-            Ok(activity) => {
-                activities.push(activity);
-                true
-            }
-            _ => false,
-        },
-        "remover atividade" => {
-            print_tasks(&activities);
-            activities.remove(select_activity());
-            true
-        }
-        "listar atividades" => {
-            print_tasks(&activities);
-            true
-        }
-        "exportar html" => match export_to_html(&activities[select_activity()]) {
-            Ok(_) => true,
-            _ => false,
-        },
-        "add tarefa" => {
-            print_tasks(&activities);
-            match create_issue(&mut activities[select_activity()]) {
-                Ok(_) => true,
-                _ => false,
-            }
-        }
-        "salvar" => match save_to_file(&activities) {
-            Ok(_) => true,
-            _ => false,
-        },
-        "sair" => false,
-        _ => true,
-    }
+    save_to_file(&workplans).unwrap();
 }
